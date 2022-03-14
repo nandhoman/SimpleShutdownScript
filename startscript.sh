0197724d5bfd9b8e39ec95c3a92b6b5e89878049
nandhoman <55098377+nandhoman@users.noreply.github.com>
2022-03-14 13:56:21 +0100

    Delete echos

--- startscript.sh
+++ startscript.sh
@@ -5,10 +5,8 @@ TARGET_HOST="172.31.1.3"
 
 count=$(ping -c 3 $TARGET_HOST | grep 64* | wc -l)
 
-echo $count
 if [ $count -gt 1 ]; then
     echo "true" > /var/VSMAlive.log
-    echo "alive"
 else
     echo "$(date) ===-> OK! "
 fi
